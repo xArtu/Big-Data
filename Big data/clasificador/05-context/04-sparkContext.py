39c3973b8b2ad2c23e5367ef51e2187422c38936
Arturo Rojas <55105578+xArtu@users.noreply.github.com>
2024-09-25 11:37:16 -0600

    Delete Big data/clasificador directory

--- Big data/clasificador/05-context/04-sparkContext.py
+++
@@ -1,7 +0,0 @@
-from pyspark import SparkContext
-sc = SparkContext("local[*]", "Ejercicio") # [n] -> nucleos para trabajar
-
-rdd = sc.parallelize(range(1, 100000000), numSlices=4) # -> doble del numero de nucleos de la PC 
-suma = rdd.reduce(lambda x,y : x+y)
-print(suma)
-sc.stop()
